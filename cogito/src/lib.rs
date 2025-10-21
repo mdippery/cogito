f036b600250e665694656a9944468ea03dfea300
Michael Dippery <michael@monkey-robot.com>
2025-10-21 09:49:17 -0700

    Copy AI interfaces into repository
    This code was extracted from my usaidwat project. It is generally useful
    across many applications, so I'm refactoring it into a general-use
    crate.

--- cogito/src/lib.rs
+++ cogito/src/lib.rs
@@ -1,14 +1,31 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2025 Michael Dippery <michael@monkey-robot.com>
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// TODO: Library docs
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub mod client;
+pub mod service;
+
+use std::fmt::Debug;
+
+/// An AI model specification.
+pub trait AIModel: Clone + Copy + Default + Debug {
+    /// The service's standard or default model.
+    ///
+    /// Often this is the same as the [best](AIModel::best()), but
+    /// there is no guarantee that is true.
+    fn flagship() -> Self;
+
+    /// The "best" model available for a given LLM.
+    ///
+    /// "Best" is obviously subjective, but generally this is the model
+    /// that offers the best price/performance ratio, and is what its
+    /// provider has defined to be the "best".
+    fn best() -> Self;
+
+    /// The least expensive model available for a given LLM.
+    fn cheapest() -> Self;
+
+    /// The fastest model available for a given LLM.
+    fn fastest() -> Self;
 }
