07b3b39ac2ec825dd04f6b72b497c00420e1426e
Michael Dippery <michael@monkey-robot.com>
2025-10-21 10:19:22 -0700

    Copy OpenAI API implementation into repository
    This code was extracted from my usaidwat project. It is generally useful
    across many applications, so I'm refactoring it into a general-use
    crate.

--- cogito-openai/src/lib.rs
+++ cogito-openai/src/lib.rs
@@ -1,14 +1,140 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2025 Michael Dippery <michael@monkey-robot.com>
+
+// TODO: Library docs.
+
+pub mod client;
+
+use cogito::AIModel;
+use serde::{Deserialize, Serialize};
+use std::fmt;
+
+/// Available OpenAI GPT models.
+///
+/// For more information on the differences between each model, see the
+/// [OpenAI model documentation](https://platform.openai.com/docs/models).
+///
+/// The [default](OpenAIModel::default()) is [gpt-4o](OpenAIModel::Gpt4o),
+/// which OpenAI describes as "the best model to use for most tasks".
+/// [According to its docs][1], [gpt-4.1](OpenAIModel::Gpt4_1) "offers a solid
+/// combination of intelligence, speed, and cost effectiveness". If you are on
+/// a budget, consider using [gpt-4.1-nano](OpenAIModel::Gpt4_1nano), the
+/// [least expensive](OpenAIModel::cheapest()) model.
+///
+/// # Cost
+///
+/// OpenAI API usage has a cost, and the cost of each model differs;
+/// naturally more powerful models cost more to use.
+///
+/// See the [cost breakdown](self#Cost) in the `openai` module documentation
+/// for more details,  or visit OpenAI's
+/// [pricing](https://platform.openai.com/docs/pricing) docs for the last prices.
+///
+/// [1]: https://platform.openai.com/docs/guides/text?api-mode=responses#choosing-a-model
+#[derive(Clone, Copy, Debug, Default, PartialEq, Deserialize, Serialize)]
+pub enum OpenAIModel {
+    /// OpenAI's flagship model for coding, reasoning, and agentic tasks
+    /// across domains.
+    #[default]
+    #[serde(rename = "gpt-5")]
+    Gpt5,
+
+    /// A faster, more cost-efficient version of [`GPT-5`](OpenAIModel::Gpt5).
+    ///
+    /// It's great for well-defined tasks and precise prompts.
+    #[serde(rename = "gpt-5-mini")]
+    Gpt5mini,
+
+    /// Fastest, cheapest version of [`GPT-5`](OpenAIModel::Gpt5).
+    ///
+    /// It's great for summarization and classification tasks.
+    #[serde(rename = "gpt-5-nano")]
+    Gpt5nano,
+
+    /// Versatile, high-intelligence flagship model.
+    #[serde(rename = "gpt-4o")]
+    Gpt4o,
+
+    /// A fast, affordable model for focused tasks.
+    #[serde(rename = "gpt-4o-mini")]
+    Gpt4omini,
+
+    /// The flagship model for complex tasks.
+    ///
+    /// It is well-suited for problem-solving across domains.
+    #[serde(rename = "gpt-4.1")]
+    Gpt4_1,
+
+    /// Provides a balance between intelligence, speed, and cost.
+    ///
+    /// An attractive model for many use cases.
+    #[serde(rename = "gpt-4.1-mini")]
+    Gpt4_1mini,
+
+    /// The fastest, most cost-effective 4.1 model.
+    #[serde(rename = "gpt-4.1-nano")]
+    Gpt4_1nano,
+
+    /// Optimized for fast, effective reasoning with exceptionally efficient
+    /// performance in coding and visual tasks.
+    #[serde(rename = "o4-mini")]
+    O4mini,
+
+    /// A well-rounded and powerful reasoning model across domains.
+    ///
+    /// It sets a new standard for math, science, coding, and visual
+    /// reasoning tasks, and excels at technical writing and following
+    /// instructions.
+    #[serde(rename = "o3")]
+    O3,
+
+    /// A mini version of the o3 model, providing high intelligence with
+    /// the same cost and latency targets of o1-mini.
+    #[serde(rename = "o3-mini")]
+    O3mini,
+
+    /// Like the o3 model, but it uses more compute to think even harder.
+    #[serde(rename = "o3-pro")]
+    O3pro,
+
+    /// A model trained with reinforcement learning that thinks before it
+    /// answers and produces a long chain of thought before responding to
+    /// the user.
+    #[serde(rename = "o1")]
+    O1,
+
+    /// A version of the [`o1`](OpenAIModel::O1) model that thinks even harder
+    /// before responding.
+    #[serde(rename = "o1-pro")]
+    O1pro,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl AIModel for OpenAIModel {
+    /// OpenAI's standard model.
+    fn flagship() -> Self {
+        OpenAIModel::default()
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    /// The "best" GPT model as defined by OpenAI.
+    fn best() -> Self {
+        OpenAIModel::default()
+    }
+
+    fn cheapest() -> Self {
+        OpenAIModel::Gpt5nano
+    }
+
+    fn fastest() -> Self {
+        // GPT 4.1-nano is noticeably faster than GPT 5-nano.
+        OpenAIModel::Gpt4_1nano
+    }
+}
+
+impl fmt::Display for OpenAIModel {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let s = serde_json::to_string(&self)
+            .unwrap_or_else(|_| panic!("could not serialize {:?}", self));
+        let s = s.trim_matches('"');
+        f.write_fmt(format_args!("{}", s))
     }
 }
---
+++ cogito-openai/tests/openai_service_https.rs
@@ -0,0 +1,49 @@
+// These tests aren't particularly interesting and mostly serve to ensure
+// that we can actually connect to the OpenAI service. Somewhat redundant
+// with openai_client_https, but it ensures we are testing the integration
+// of each individual component.
+
+use cogito::client::AIRequest;
+use cogito::service::Service;
+use cogito_openai::OpenAIModel;
+use cogito_openai::client::{OpenAIRequest, OpenAIResponse};
+use hypertyper::{Auth, HTTPClientFactory, HTTPPost, HTTPResult};
+
+#[tokio::test]
+async fn it_sends_a_post_request_using_gpt4o() {
+    let auth =
+        Auth::from_env("OPENAI_API_KEY").expect("Could not create auth. Is $OPENAI_API_KEY set?");
+    let request = OpenAIRequest::default()
+        .model(OpenAIModel::Gpt4o)
+        .input("write a haiku about ai");
+    let factory = HTTPClientFactory::new(env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+    let service = Service::new(factory);
+    let response: HTTPResult<OpenAIResponse> = service
+        .post("https://api.openai.com/v1/responses", &auth, &request)
+        .await;
+    let resp = response.expect("could not make OpenAI API request");
+    assert_eq!(resp.output().count(), 1);
+    assert_eq!(resp.output().next().unwrap().content().count(), 1);
+}
+
+#[tokio::test]
+async fn it_sends_a_post_request_using_gpt5nano() {
+    let auth =
+        Auth::from_env("OPENAI_API_KEY").expect("Could not create auth. Is $OPENAI_API_KEY set?");
+    let request = OpenAIRequest::default()
+        .model(OpenAIModel::Gpt5nano)
+        .input("write a haiku about ai");
+    let factory = HTTPClientFactory::new(env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+    let service = Service::new(factory);
+    let response: HTTPResult<OpenAIResponse> = service
+        .post("https://api.openai.com/v1/responses", &auth, &request)
+        .await;
+    let resp = response.expect("could not make OpenAI API request");
+    assert_eq!(resp.output().count(), 2);
+
+    let output = resp.output().nth(1).expect(&format!(
+        "could not get second element of output: {:?}",
+        resp
+    ));
+    assert_eq!(output.content().count(), 1);
+}
